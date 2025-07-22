6daf4b5b9ed1b8b467afac50c887ba77ccf16579
Santhosh <santhoshs2220@gmail.com>
2025-07-22 10:58:54 +0530

    changed the bg

---
+++ script.js
@@ -0,0 +1,6 @@
+/*let modelname;
+
+document.getElementById("Mysubmit").onclick=function(){
+    modelname=document.getElementById("modelname").value;
+    document.getElementById("myp").textContent=`hi this model is ${modelname}`
+};
